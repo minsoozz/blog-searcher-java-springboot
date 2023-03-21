97e6daba1e9fd507d17b11356df97d7a7eb26b11
minsoozz <minsoo1604@naver.com>
2023-03-22 02:23:23 +0900

    feat: 공통 응답 객체 구현

---
+++ blogsearch-presentation/src/main/java/com/github/minsoozz/search/popularsearch/dto/BaseResponseDto.java
@@ -0,0 +1,13 @@
+package com.github.minsoozz.search.popularsearch.dto;
+
+/**
+ * @author minsoozz
+ * @date 2023.03.22
+ */
+public record BaseResponseDto<T>(String message, T data) {
+
+    public static <T> BaseResponseDto<T> ok(T data) {
+        return new BaseResponseDto<>("success", data);
+    }
+
+}
