30ce17c5eafa6446cfcdd7d3489cf61c9391a01b
minsoozz <minsoo1604@naver.com>
2023-03-22 12:04:58 +0900

    test: 인기검색어 업데이트 테스트 코드 작성
    issue: #11

---
+++ blogsearch-infrastructure/src/test/java/com/github/minsoozz/search/config/TestJpaConfiguration.java
@@ -0,0 +1,19 @@
+package com.github.minsoozz.search.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestJpaConfiguration {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
--- blogsearch-infrastructure/src/test/java/com/github/minsoozz/search/persistence/service/PopularSearchQueryTest.java
+++ blogsearch-infrastructure/src/test/java/com/github/minsoozz/search/persistence/service/PopularSearchQueryTest.java
@@ -2,52 +2,83 @@ package com.github.minsoozz.search.persistence.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
-import com.github.minsoozz.search.exception.PopularSearchEntityNotFoundException;
+import com.github.minsoozz.search.config.TestJpaConfiguration;
+import com.github.minsoozz.search.persistence.dto.PopularSearchDto;
 import com.github.minsoozz.search.persistence.entity.PopularSearchJpaEntity;
 import com.github.minsoozz.search.persistence.repository.popularsearch.PopularSearchRepository;
-import java.util.Optional;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.util.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@Import({TestJpaConfiguration.class})
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 class PopularSearchQueryTest {
 
-    @InjectMocks
-    private PopularSearchQuery popularSearchQuery;
-
-    @Mock
-    private PopularSearchRepository popularSearchRepository;
-
-    private final String query = "minsoo";
+    @Autowired
+    PopularSearchRepository popularSearchRepository;
 
     @Test
-    void 이미_존재하는_엔티티를_찾는다() {
+    void 키워드가_존재하는지_조회한다() {
         // given
-        PopularSearchJpaEntity savedPopularSearchJpa = PopularSearchJpaEntity.of(query);
+        final String query = "민수";
 
         // when
-        when(popularSearchRepository.findByKeyword(query)).thenReturn(Optional.of(savedPopularSearchJpa));
-        PopularSearchJpaEntity popularSearchJpa = popularSearchQuery.findByKeyword(query)
-            .orElseThrow(PopularSearchEntityNotFoundException::new);
+        boolean exists = popularSearchRepository.existsByKeyword(query);
 
         // then
-        assertEquals(query, popularSearchJpa.getKeyword());
+        assertTrue(exists);
     }
 
     @Test
-    void 존재하지_않는_엔티티를_찾는다() {
+    void 키워드로_엔티티를_조회한다() {
+        // given
+        final String query = "은행";
 
         // when
-        when(popularSearchRepository.findByKeyword(query)).thenReturn(Optional.empty());
-        Optional<PopularSearchJpaEntity> popularSearchJpa = popularSearchQuery.findByKeyword(query);
+        PopularSearchJpaEntity popularSearchJpaEntity = popularSearchRepository.findByKeyword(query).orElseThrow();
 
         // then
-        assertTrue(ObjectUtils.isEmpty(popularSearchJpa));
+        assertEquals(query, popularSearchJpaEntity.getKeyword());
+    }
+
+    @Test
+    void 인기검색어_최대_10개를_조회한다() {
+        // given
+        final long count = 10;
+
+        // when
+        List<PopularSearchDto> keywords = popularSearchRepository.findTop10Keywords();
+
+        // then
+        assertEquals(count, keywords.size());
+    }
+
+    @Test
+    void 인기검색어_순위가_정확한지_검증한다() {
+        // when
+        List<PopularSearchDto> keywords = popularSearchRepository.findTop10Keywords();
+
+        assertEquals(10, keywords.size());
+        assertEquals("투자상품", keywords.get(0).keyword());
+        assertEquals("은행", keywords.get(1).keyword());
+        assertEquals("외환", keywords.get(2).keyword());
+        assertEquals("보험상품", keywords.get(3).keyword());
+        assertEquals("민수", keywords.get(4).keyword());
+        assertEquals("신용카드", keywords.get(5).keyword());
+        assertEquals("주식", keywords.get(6).keyword());
+        assertEquals("부동산대출", keywords.get(7).keyword());
+        assertEquals("카드론", keywords.get(8).keyword());
+        assertEquals("대출", keywords.get(9).keyword());
     }
 }
